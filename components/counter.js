5eb8ac4300e3713331bd6e688e03acb3fe0be410
Jason Mavandi <jason.mavandi@emc.com>
2017-07-28 14:17:13 -0600

    Correctly name div

--- components/counter.js
+++ components/counter.js
@@ -1,7 +1,7 @@
 // component/counter.js
 export default {
     count: 0,
-    view: vnode => m('div', [
+    view: vnode => m('', [
         m('button', {
             onclick: _ => vnode.state.count++
         }, vnode.attrs.name || 'Count'),
